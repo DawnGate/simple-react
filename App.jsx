572018326e2672a59e7d5d76d074f259e1894201
DawnGate <lam99quyen@gmail.com>
2024-01-25 21:08:09 +1200

    update: a little bit of change for better debug

--- App.jsx
+++ App.jsx
@@ -2,9 +2,17 @@ import React from "react"
 
 const App = () => {
 	console.log("Hello");
-	return <div>
-		<h1>Hello World!!!!</h1>
-	</div>
+	return(
+		<div>
+			<div>
+				<h1>Hello World!!!!</h1>
+			</div>
+			<div>
+					<span>Label</span>
+					<button>Hello</button>
+			</div>
+		</div>
+	)
 }
 
 export default App
---
+++ README.md
@@ -0,0 +1,6 @@
+# This is a project for read react source code and create a simple version of react
+
+
+
+author: lam99quyen@gmail.com
+version: React, react-dom 18
